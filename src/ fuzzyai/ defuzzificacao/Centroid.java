89fe1c5c9706d4a6a43c027608ca3424921c4310
Ítalo Conceição <32005210+cisant@users.noreply.github.com>
2017-11-29 19:24:24 -0200

    Update Centroid
    codigo de defuzzyficacao

--- src/ fuzzyai/ defuzzificacao/Centroid.java
+++ src/ fuzzyai/ defuzzificacao/Centroid.java
@@ -8,8 +8,22 @@ import java.util.List;
 public final class Centroid implements IDefuzzificacao{
 
     @Override
-    public VariavelFuzzyficada defuzzificar() throws Exception {
-        List<Point2D.Double> pontos = new ArrayList<>();
+    public VariavelFuzzyficada defuzzificar(List<Point2D.Double> pontos) throws Exception {
+		
+        Double result1=0;
+		Double result2 = 0;
+		Double resultFinal = 0;
+		
+		foreach(Point2D.Double ponto : pontos){
+			
+			result += ponto.valor * ponto.valor2;
+			result2 += ponto.valor2;
+		}
+		
+		resultFinal = result1/result2;
+		
+		
+		List<Point2D.Double> pontos = new ArrayList<>();
         pontos.add(new Point2D.Double(0, 0.66));
         pontos.add(new Point2D.Double(30, 0.66));
         pontos.add(new Point2D.Double(40, 0.33));
