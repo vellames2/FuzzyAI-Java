1d7e3b5328725cef76b89fce2918858840bb9dbe
Cassiano Vellames <c.vellames@outlook.com>
2017-12-01 15:56:06 -0300

    link para acesso dos manuais e alteração do salvamento do arquivo

--- src/fuzzyai/FuzzyAI.java
+++ src/fuzzyai/FuzzyAI.java
@@ -1,6 +1,7 @@
 package fuzzyai;
 
 import fuzzyai.defuzzificacao.IDefuzzificacao;
+import fuzzyai.fuzzificacao.FuzzificacaoPadrao;
 import fuzzyai.inferencia.IInferencia;
 import fuzzyai.inferencia.VariavelFuzzyficada;
 import java.awt.BorderLayout;
@@ -24,32 +25,37 @@ import java.util.List;
 import javax.swing.JOptionPane;
 import fuzzyai.fuzzificacao.IFuzzificacao;
 import fuzzyai.inferencia.Imagem;
+import java.awt.GridLayout;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public final class FuzzyAI {
-    
+
     public static int largura = 800;
     public static int altura = 600;
 
-    public static void main(String[] args){   
+    public static void main(String[] args) {
         // Tela da aplicação
         JFrame telaPrincipal = FuzzyAI.criarTelaPrincipal();
-        
+
         // Painel principal da aplicação
         JPanel painelPrincipal = new JPanel(new BorderLayout());
-        
+
         // Painel com as ações de carregar JSON
         JPanel painelImportarJSON = FuzzyAI.painelImportarJSON();
-        
+
         // Painel de ajuda
         JPanel painelAjuda = FuzzyAI.painelAjuda();
-        
+
         // Painel com botões
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.ipadx = 30;
         constraints.ipady = 10;
         constraints.insets = new Insets(5, 10, 5, 0);
         constraints.anchor = GridBagConstraints.NORTHWEST;
-        
+
         JPanel painelBotoes = new JPanel(new GridBagLayout());
         JButton btnImportarJSON = new JButton("Importar JSON");
         btnImportarJSON.addActionListener(new ActionListener() {
@@ -57,23 +63,23 @@ public final class FuzzyAI {
             public void actionPerformed(ActionEvent e) {
                 BorderLayout layout = (BorderLayout) painelPrincipal.getLayout();
                 Component componente = layout.getLayoutComponent(BorderLayout.CENTER);
-                if(componente != null) {
+                if (componente != null) {
                     painelPrincipal.remove(componente);
                 }
 
                 painelPrincipal.add(painelImportarJSON, BorderLayout.CENTER);
                 painelPrincipal.revalidate();
-                painelPrincipal.repaint();                
+                painelPrincipal.repaint();
             }
         });
-        
+
         JButton btnAjuda = new JButton("Ajuda");
         btnAjuda.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 BorderLayout layout = (BorderLayout) painelPrincipal.getLayout();
                 Component componente = layout.getLayoutComponent(BorderLayout.CENTER);
-                if(componente != null) {
+                if (componente != null) {
                     painelPrincipal.remove(componente);
                 }
 
@@ -82,38 +88,38 @@ public final class FuzzyAI {
                 painelPrincipal.repaint();
             }
         });
-        
+
         constraints.gridx = 0;
         constraints.gridy = 0;
         constraints.weightx = 0;
         constraints.weighty = 0;
         painelBotoes.add(btnImportarJSON, constraints);
-        
+
         constraints.gridx = 0;
         constraints.gridy = 1;
         constraints.weightx = 1;
         constraints.weighty = 1;
         painelBotoes.add(btnAjuda, constraints);
-        
+
         // Adiciona paineis ao painel principal
         painelPrincipal.add(painelBotoes, BorderLayout.WEST);
-        
-        
+
         // Adiciona painel prinicpal ao JFrame
         telaPrincipal.add(painelPrincipal);
         telaPrincipal.setVisible(true);
     }
-    
+
     public static JFrame criarTelaPrincipal() {
         JFrame telaPrincipal = new JFrame("Sistema Fuzzy");
-        telaPrincipal.setSize(largura, altura);        
+        telaPrincipal.setSize(largura, altura);
         telaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         telaPrincipal.setResizable(false);
         return telaPrincipal;
     }
-   
+
     /**
      * Monta o painel de importação do JSON
+     *
      * @return Retorna o painel do JSON
      */
     public static JPanel painelImportarJSON() {
@@ -121,15 +127,15 @@ public final class FuzzyAI {
         constraints.insets = new Insets(5, 0, 0, 0);
         constraints.anchor = GridBagConstraints.NORTH;
         JPanel importarJSON = new JPanel(new GridBagLayout());
-        
+
         // Texto
         JLabel textoImportacao = new JLabel("Importar JSON");
-        
+
         // Input para adicionar o caminho do JSON
         JTextField inputCaminhoJSON = new JTextField(40);
-        inputCaminhoJSON.setText("/home/vellames/Documents/Faculdade/Inteligencia Artificial/FuzzyAI/FuzzyAI/config.json");
+        //inputCaminhoJSON.setText("/home/vellames/Documents/Faculdade/Inteligencia Artificial/FuzzyAI/FuzzyAI/config.json");
         inputCaminhoJSON.setEditable(false);
-        
+
         // Botão de importação
         JButton btnImportar = new JButton("Importar");
         //btnImportar.setEnabled(false);
@@ -141,7 +147,7 @@ public final class FuzzyAI {
                 FuzzyAI.abrirTelaFuzzy(caminhoJSON);
             }
         });
-         
+
         // Botão para procurar arquivo
         JButton btnProcurar = new JButton("Procurar");
         btnProcurar.addActionListener(new ActionListener() {
@@ -151,16 +157,16 @@ public final class FuzzyAI {
                 // Abre o selecionador de arquivo
                 JFileChooser selecionarArquivo = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
                 int retorno = selecionarArquivo.showOpenDialog(null);
-                if(retorno == JFileChooser.APPROVE_OPTION) {
+                if (retorno == JFileChooser.APPROVE_OPTION) {
                     File arquivoSelecionado = selecionarArquivo.getSelectedFile();
                     String caminhoAbsoluto = arquivoSelecionado.getAbsolutePath();
                     String nome = arquivoSelecionado.getName();
-                    
+
                     // Verifica a extensão do arquivo, se valida, seta no Jtextinput
                     int i = nome.lastIndexOf('.');
-                    if(i > 0) {
+                    if (i > 0) {
                         String extensao = nome.substring(i + 1);
-                        if(extensao.toLowerCase().equals("json")) {
+                        if (extensao.toLowerCase().equals("json")) {
                             inputCaminhoJSON.setText(caminhoAbsoluto);
                             btnImportar.setEnabled(true);
                         } else {
@@ -172,49 +178,85 @@ public final class FuzzyAI {
                 }
             }
         });
-        
+
         // Adiciona elementos ao painel
         constraints.gridx = 0;
         constraints.gridy = 0;
         constraints.weighty = 0;
         importarJSON.add(textoImportacao, constraints);
-        
+
         constraints.gridx = 0;
         constraints.gridy = 1;
         constraints.weighty = 1;
         importarJSON.add(inputCaminhoJSON, constraints);
-        
+
         constraints.gridx = 1;
         constraints.gridy = 1;
         constraints.weighty = 1;
         constraints.insets = new Insets(0, 5, 0, 0);
         importarJSON.add(btnProcurar, constraints);
-        
+
         constraints.gridx = 0;
         constraints.gridy = 2;
         constraints.weighty = 45;
         importarJSON.add(btnImportar, constraints);
-        
+
         return importarJSON;
     }
-    
+
+    //Painel de Ajuda
     public static JPanel painelAjuda() {
+        //Cria o botao com link do JavaDOC
         JPanel painelAjuda = new JPanel();
-        
-        JLabel label = new JLabel("Ajuda");
-        
-        painelAjuda.add(label);
-        
+        JButton btnJava = new JButton("JavaDOC");
+        btnJava.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    java.awt.Desktop.getDesktop().browse(new java.net.URI("http://www.google.com"));
+                } catch (URISyntaxException ex) {
+                    Logger.getLogger(FuzzyAI.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(FuzzyAI.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        //Cria o botao com link do Manual do Usuário
+        JButton btnManualUsuario = new JButton("Manual do Usuário");
+        btnManualUsuario.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    java.awt.Desktop.getDesktop().browse(new java.net.URI("https://drive.google.com/open?id=1C2YqAfvfAXzL0HipPHmlQ7p9X9AO4pkC"));
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Não foi possível abrir o browser, por favor acesse o link abaixo para ter acesso ao conteudo:\nhttps://drive.google.com/open?id=1C2YqAfvfAXzL0HipPHmlQ7p9X9AO4pkC");
+                }
+            }
+        });
+        //Cria o botao com link do Manual do JSON
+        JButton btnManualJSON = new JButton("Manual JSON");
+        btnManualJSON.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    java.awt.Desktop.getDesktop().browse(new java.net.URI("https://drive.google.com/open?id=1UdgbVKsxcbN0k0xYQXEoZirL5rjQhomc"));
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Não foi possível abrir o browser, por favor acesse o link abaixo para ter acesso ao conteudo:\nhttps://drive.google.com/open?id=1UdgbVKsxcbN0k0xYQXEoZirL5rjQhomc");
+                }
+            }
+        });
+        //Insere os elementos no painel
+        painelAjuda.add(btnJava);
+        painelAjuda.add(btnManualUsuario);
+        painelAjuda.add(btnManualJSON);
+
         return painelAjuda;
     }
-    
-    public static void abrirTelaFuzzy(String caminho){
+
+    public static void abrirTelaFuzzy(String caminho) {
         // Variaveis de controle
         JPanel painelFuzzy = new JPanel();
         painelFuzzy.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 15));
         ModeloFuzzy modeloFuzzy = new ModeloFuzzy();
         List<JTextField> campos = new ArrayList<>();
-        
+
         // Carrega os campos
         try {
             modeloFuzzy.carregarCampos(caminho);
@@ -223,21 +265,21 @@ public final class FuzzyAI {
             JOptionPane.showMessageDialog(null, e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
             return;
         }
-        
+
         // Adiciona o label e os campos no panel
-        for(String campo : modeloFuzzy.getOrdemEntrada()) {
+        for (String campo : modeloFuzzy.getOrdemEntrada()) {
             JLabel jlabel = new JLabel(campo);
             JTextField textField = new JTextField(17);
             campos.add(textField);
             painelFuzzy.add(jlabel);
             painelFuzzy.add(textField);
         }
-        
+
         // Cria JFrame
         JFrame telaFuzzy = new JFrame("Inserir Valores");
-        telaFuzzy.setSize(largura, altura);        
+        telaFuzzy.setSize(largura, altura);
         telaFuzzy.setResizable(false);
-        
+
         // Cria Painel com os botões
         JPanel painelAcoes = new JPanel();
         JButton enviar = new JButton("Enviar");
@@ -246,18 +288,18 @@ public final class FuzzyAI {
             public void actionPerformed(ActionEvent e) {
                 boolean possuiErros = false;
                 List<Double> valoresEntrada = new ArrayList<>();
-                
+
                 // Validação dos campos e inserção no array de valores de entrada;
-                for(int i = 0; i < campos.size(); i++) {
+                for (int i = 0; i < campos.size(); i++) {
                     // Verifica se os campos estão preenchidos
                     JTextField campo = campos.get(i);
-                    if(campo.getText().isEmpty()) {
+                    if (campo.getText().isEmpty()) {
                         String msg = "Preencha o campo " + modeloFuzzy.getOrdemEntrada().get(i);
                         JOptionPane.showMessageDialog(null, msg, "Erro", JOptionPane.ERROR_MESSAGE);
                         possuiErros = true;
                         break;
                     }
-                    
+
                     // Verifica se os campos são double
                     try {
                         valoresEntrada.add(Double.parseDouble(campo.getText()));
@@ -268,11 +310,11 @@ public final class FuzzyAI {
                         break;
                     }
                 }
-                
-                if(possuiErros) {
+
+                if (possuiErros) {
                     return;
                 }
-                
+
                 try {
                     // Carrega o restante do modelo fuzzy
                     modeloFuzzy.carregarModeloFuzzy();
@@ -280,17 +322,17 @@ public final class FuzzyAI {
                     // Fuzzificacao
                     IFuzzificacao fuzzyficacao = (IFuzzificacao) Class.forName(ConfiguracoesSistema.PACOTE_FUZZIFICACAO + "." + modeloFuzzy.getModoFuzzificacao()).newInstance();
                     List<VariavelFuzzyficada> variaveisFuzzyficadas = fuzzyficacao.fuzzificar(valoresEntrada, modeloFuzzy);
-                    
+
                     // Inferencia
                     IInferencia inferencia = (IInferencia) Class.forName(ConfiguracoesSistema.PACOTE_INFERENCIA + "." + modeloFuzzy.getModoInferencia()).newInstance();
                     Imagem imagem = inferencia.inferir(variaveisFuzzyficadas, modeloFuzzy);
-                    
+
                     // Defuzzificacao
                     IDefuzzificacao defuzzificacao = (IDefuzzificacao) Class.forName(ConfiguracoesSistema.PACOTE_DEFUZZIFICACAO + "." + modeloFuzzy.getModoDefuzzificacao()).newInstance();
-                    VariavelFuzzyficada variavel =  defuzzificacao.defuzzificar(imagem, modeloFuzzy, fuzzyficacao);
-                    
-                    TelaResultado telaResultado = new TelaResultado(modeloFuzzy, variaveisFuzzyficadas, imagem, variavel);
-                    
+                    VariavelFuzzyficada variavel = defuzzificacao.defuzzificar(imagem, modeloFuzzy, fuzzyficacao);
+
+                    TelaResultado telaResultado = new TelaResultado(valoresEntrada ,modeloFuzzy, variaveisFuzzyficadas, imagem, variavel);
+
                     System.out.println(variavel.toString());
                 } catch (Exception ex) {
                     ex.printStackTrace();
@@ -298,7 +340,7 @@ public final class FuzzyAI {
                 }
             }
         });
-        
+
         JButton cancelar = new JButton("Cancelar");
         cancelar.addActionListener(new ActionListener() {
             @Override
@@ -308,14 +350,14 @@ public final class FuzzyAI {
         });
         painelAcoes.add(enviar);
         painelAcoes.add(cancelar);
-        
+
         // Cria painel que agrupa todos os layouts
         JPanel painelInserirValores = new JPanel(new BorderLayout());
         painelInserirValores.add(painelFuzzy, BorderLayout.CENTER);
         painelInserirValores.add(painelAcoes, BorderLayout.SOUTH);
-        
+
         telaFuzzy.add(painelInserirValores);
-        
+
         // Exibe JFrame
         telaFuzzy.setVisible(true);
     }
--- src/fuzzyai/TelaResultado.java
+++ src/fuzzyai/TelaResultado.java
@@ -12,13 +12,23 @@ import fuzzyai.inferencia.VariavelFuzzyficada;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Point2D;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**
@@ -27,72 +37,117 @@ import javax.swing.JPanel;
 public final class TelaResultado extends JFrame {
     /**
      * Construtor da classe da tela de resultado
+     * @param valoresEntrada Valores usados como entrada de dados
      * @param modeloFuzzy Modelo fuzzy com os dados carregados
      * @param resultadoFuzzificacao Resultado da fuzzificacao
      * @param resultadoInferencia Resultado da inferencia
      * @param resultadoDefuzzificacao Resultado da defuzzificacao
      */
-    public TelaResultado(ModeloFuzzy modeloFuzzy, List<VariavelFuzzyficada> resultadoFuzzificacao, Imagem resultadoInferencia, VariavelFuzzyficada resultadoDefuzzificacao) {
+    public TelaResultado(List<Double> valoresEntrada, ModeloFuzzy modeloFuzzy, List<VariavelFuzzyficada> resultadoFuzzificacao, Imagem resultadoInferencia, VariavelFuzzyficada resultadoDefuzzificacao) {
         this.setTitle("Resultado - " + modeloFuzzy.getNome());
         this.setSize(largura, altura);        
         this.setResizable(false);
         
         JPanel painel = new JPanel(new BorderLayout());
-        
+       
         JPanel painelFuzzificacao = new JPanel();
         painelFuzzificacao.setLayout(new BoxLayout(painelFuzzificacao, BoxLayout.Y_AXIS));
-        
+       
         JPanel painelInferencia = new JPanel();
         painelInferencia.setLayout(new BoxLayout(painelInferencia, BoxLayout.Y_AXIS));
-        
+       
         JPanel painelDefuzzificacao = new JPanel();
         painelDefuzzificacao.setLayout(new BoxLayout(painelDefuzzificacao, BoxLayout.Y_AXIS));
         
+        String txt = "";
+       
+        txt += ("+-------------- Valores de Entrada --------------+%n");
+        List<String> ordemEntrada = modeloFuzzy.getOrdemEntrada();
+        for(int i = 0; i < ordemEntrada.size(); i++) {
+            txt += ordemEntrada.get(i) + " - " + valoresEntrada.get(i) + "%n";
+        }
         // Fuzzificacao
+        txt += ("+--------------Fuzzificação--------------+%n");
         painelFuzzificacao.add(new JLabel("Fuzzificacao: "));
         for(VariavelFuzzyficada variavelFuzzyficada : resultadoFuzzificacao) {
             painelFuzzificacao.add(Box.createRigidArea(new Dimension(0,10)));
             painelFuzzificacao.add(new JLabel(variavelFuzzyficada.getVariavelFuzzy().getNome()));
+            //GRAVA NOME DA VARIAVEL
+            txt += (variavelFuzzyficada.getVariavelFuzzy().getNome()+"%n");
             painelFuzzificacao.add(Box.createRigidArea(new Dimension(0,10)));
             Object[] keys = variavelFuzzyficada.getResultado().keySet().toArray();
             for(int x = 0; x < variavelFuzzyficada.getResultado().size(); x++) {
                 painelFuzzificacao.add(new JLabel(keys[x] + " - " + variavelFuzzyficada.getResultado().get(keys[x].toString())));
+                txt += (keys[x] + " - " + variavelFuzzyficada.getResultado().get(keys[x].toString())+"%n");
             }
+            txt += ("%n");
         }
-        
+        txt += ("+---------------------------------------+%n");
+       
         // Inferencia
+        txt += ("+--------------Inferencia---------------+%n");
         painelInferencia.add(new JLabel("Inferencia: "));
         painelInferencia.add(Box.createRigidArea(new Dimension(0,10)));
         for(Point2D.Double ponto : resultadoInferencia.getPontos()) {
             painelInferencia.add(new JLabel("[" + ponto.getX() + " , " + ponto.getY() + "]"));
             painelInferencia.add(Box.createRigidArea(new Dimension(0,10)));
+            txt += ("[" + ponto.getX() + " , " + ponto.getY() + "]%n");
         }
-        
+        txt += ("+---------------------------------------+%n");
+       
         // Defuzzificacao
+        txt += ("+--------------Defuzzificação-----------+%n");
         painelDefuzzificacao.add(new JLabel("Defuzzificacao: "));
         painelDefuzzificacao.add(Box.createRigidArea(new Dimension(0,10)));
         painelDefuzzificacao.add(new JLabel(resultadoDefuzzificacao.getVariavelFuzzy().getNome()));
+        txt += (resultadoDefuzzificacao.getVariavelFuzzy().getNome()+"%n");
         painelDefuzzificacao.add(Box.createRigidArea(new Dimension(0,10)));
         Object[] keys = resultadoDefuzzificacao.getResultado().keySet().toArray();
         for(int x = 0; x < resultadoDefuzzificacao.getResultado().size(); x++) {
             painelDefuzzificacao.add(new JLabel(keys[x] + " - " + resultadoDefuzzificacao.getResultado().get(keys[x].toString())));
+            txt += (keys[x] + " - " + resultadoDefuzzificacao.getResultado().get(keys[x].toString())+"%n");
         }
-        
+        txt += ("+---------------------------------------+%n");
+        //IMPRIME A DATA E HORA DA EXPORTAÇAO
+        txt += ("+-----------Arquivo Exportado-----------+%n");        
+        Date dataAtual = new Date();
+        txt += (dataAtual.toString()+"%n");
+        txt += ("+---------------------------------------+%n");
+       
         JPanel painelCentro = new JPanel();
         painelCentro.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 15));
         painelCentro.add(painelFuzzificacao);
         painelCentro.add(painelInferencia);
         painelCentro.add(painelDefuzzificacao);
-        
+       
+        //BOTAO DE EXPORTAR
         JPanel painelFim = new JPanel();
         painelFim.setLayout(new FlowLayout(FlowLayout.CENTER));
         JButton exportar = new JButton("Exportar");
-        painelFim.add(exportar);
         
+        final String txtFinal = txt;
+        exportar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                FileWriter arq;
+                try {
+                    String caminho = System.getProperty("user.dir") + File.separator + "Resultado.txt";
+                    arq = new FileWriter(caminho);
+                    PrintWriter gravarArq = new PrintWriter(arq);
+                    gravarArq.printf(txtFinal);
+                    arq.close();
+                    JOptionPane.showMessageDialog(null, "Arquivo foi exportado com sucesso no diretorio " + caminho);
+                } catch (IOException ex) {
+                    Logger.getLogger(TelaResultado.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        painelFim.add(exportar);
+       
         painel.add(painelCentro, BorderLayout.CENTER);
         painel.add(painelFim, BorderLayout.SOUTH);
-        
+       
         this.add(painel);
         this.setVisible(true);
     }
+   
 }
